b96ea94a37cd39da764610c1a1c50b1f998e3024
karem <karem@carwah.co>
2023-03-15 21:51:28 +0200

    sprint 3

--- src/routes/orders/ServiceTable.js
+++ src/routes/orders/ServiceTable.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { FormattedMessage } from "react-intl"
 import { Alert } from "Constants/constants";
+import StatusDropDown from "Components/shared/StatusDropDown"
 
 const ServiceTable=({withcheckbox,service})=>{
     return(
@@ -51,7 +52,7 @@ const ServiceTable=({withcheckbox,service})=>{
               {["title","field","executive_time","cost","support_ratio","is_active"]?.map((data, index) => (
                 <>
                   <td key={JSON.stringify(index)} align={`${data?.align || ""}`}>
-                   { data == "executive_time" ? <> {service[data]}  <FormattedMessage id={service["executive_time_type"]} /> </>: data == "field" ? service[data].name : service[data]}
+                   { data == "executive_time" ? <> {service[data]}  <FormattedMessage id={service["executive_time_type"]} /> </>: data == "field" ? service[data].name : data == "is_active" ?  <StatusDropDown  activationStatus={service[data]}/>: service[data]}
                   </td>
                 </>
               ))}
